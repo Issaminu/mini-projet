42f679d6a7ef88a357a51272764f8bb18fc700f8
Issam-Boubcher <boubcher.issam@gmail.com>
2022-12-01 22:26:51 +0100

    Init Next app and starter configuration

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+};
+const intercept = require("intercept-stdout");
+function interceptStdout(text) {
+  if (
+    text.includes("Duplicate atom key") ||
+    text.includes("Fast Refresh had to perform a full reload")
+  ) {
+    return "";
+  }
+  return text;
 }
+intercept(interceptStdout);
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
+import { RecoilRoot } from "recoil";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Head>
+        <title>Mini Projet | ENSET Mohammedia</title>
+      </Head>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </SessionProvider>
+  );
 }
