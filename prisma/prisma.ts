5fd87d1008cd02aa8237f9d0fb0dce0000911444
Issam-Boubcher <boubcher.issam@gmail.com>
2022-12-08 19:55:52 +0100

    Add Dashboard

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  prisma = new dfbdfbPrismaClient();
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient();
