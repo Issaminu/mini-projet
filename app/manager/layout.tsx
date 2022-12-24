3ca4905d2010c05e40d8ce445655990153ad3aca
yg <hmz.elm@outlook.sa>
2022-12-24 23:07:23 +0100

    added list of rooms /*still need improvements*/

--- app/manager/layout.tsx
+++ app/manager/layout.tsx
@@ -1,12 +1,12 @@
-"use client";
-import React from "react";
-import Navbar from "../../components/Navbar/ManagerNavBar";
+'use client';
+import React from 'react';
+import Navbar from '../../components/Navbar/ManagerNavBar';
 
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col lg:flex-row">
       <Navbar />
-      <div className="mt-12 lg:mt-0 lg:ml-64">{children}</div>
+      <div className="mt-12 lg:mt-0 lg:ml-64 w-full ">{children}</div>
     </div>
   );
 };
---
+++ components/room/SingleRoom.tsx
@@ -0,0 +1,44 @@
+import { Fragment, useState, useCallback } from 'react';
+
+import axios from 'axios';
+import { signOut } from 'next-auth/react';
+import roomId from '../../app/manager/room/[roomId]';
+
+const singleRoom = (props) => {
+  return (
+    <Fragment>
+      <tr key={props.room.number}>
+        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+          <div className="flex items-center">
+            <div className="ml-4">
+              <div className="font-medium text-gray-900">
+                {props.room.number}
+              </div>
+            </div>
+          </div>
+        </td>
+        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+          <div className="text-gray-900">{props.room.floorId}</div>
+        </td>
+        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+          <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+            {props.room.roomState ? props.room.roomState : 'Free'}
+          </span>
+        </td>
+        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+          {props.room.typeId}
+        </td>
+        <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+          <a
+            href="#"
+            className="text-indigo-600 hover:text-indigo-900"
+          >
+            See Details
+            <span className="sr-only">, {props.room.type}</span>
+          </a>
+        </td>
+      </tr>
+    </Fragment>
+  );
+};
+export default singleRoom;
